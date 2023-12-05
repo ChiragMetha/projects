4c08e371975cbe431ee4dc03cb04fc1e1e5891d4
chirag metha <chiragmetha1805@gmail.com>
2023-12-05 18:55:56 +0530

    dict

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+thisdict={"Brand":"Volvo",
+          "year":"2023",
+        "color":["red","blue","black"]  }
+print(thisdict)
+
+
+#to see of whgich type
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+      
+#shortcut
+thisdict = dict(name = "chirag", age = 23, country = "India")
+
+print(thisdict) 
